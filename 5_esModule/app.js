8212870bb8c2eb6ddf00dd797913d655c17bdc22
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-01-14 21:23:06 +0530

    Learning es module

---
+++ 5_esModule/app.js
@@ -0,0 +1,5 @@
+import sum from "./math.js"
+import {mult,div} from "./math.js"
+console.log("The sum is :",sum(50,70))
+console.log("The sum is :",mult(50,70))
+console.log("The sum is :",div(50,70))
---
+++ 5_esModule/math.js
@@ -0,0 +1,13 @@
+const sum=(a,b)=>{
+return a+b
+}
+
+export const mult=(a,b)=>{
+return a*b
+}
+
+const div=(a,b)=>{
+return  a/b
+}
+export default sum
+export {div}

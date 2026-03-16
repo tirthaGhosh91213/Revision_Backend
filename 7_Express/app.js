850105f5fbf4180dbbb16315cae88c718f8f6250
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-03-16 21:15:56 +0530

    handle form by express

--- 7_Express/app.js
+++ 7_Express/app.js
@@ -1,16 +1,41 @@
 import express from "express"
 import { PORT } from "./env.js";
+import path from "path";
 
 const app=express();
 
-app.get("/",(req,res)=>{
-  return res.send("<h1>Hey Tirtha </h1>")
+const staticPath=path.join(import.meta.dirname,"public")
+console.log(staticPath)
+app.use(express.static(staticPath));
+app.use(express.urlencoded({extended:true})) /// extended :true for the nested object 
+
+// app.get("/",(req,res)=>{
+//   return res.send("<h1>Hey Tirtha </h1>")
+// })
+
+// app.get("/about",(req,res)=>{
+//   return res.send("<h1>I am a full stack developer</h1>")
+// })
+
+// form submission
+
+// app.get("/contact",(req,res)=>{
+//   console.log(req.query)
+//   res.redirect("/")
+// })
+
+app.post("/contact",(req,res)=>{
+  console.log(req.body)
+  res.send("OKKKKKK")
 })
 
-app.get("/about",(req,res)=>{
-  return res.send("<h1>I am a full stack developer</h1>")
+app.use((req,res)=>{
+  // res.sendFile(path.join(staticPath,"404.html")) 
+  // or 
+  res.sendFile(path.join(import.meta.dirname ,"view","404.html"))
 })
+
 // const PORT=process.env.PORT||6008;
 app.listen(PORT,()=>{
-  console.log(`Server running at port number ${PORT}`)
+  console.log(`Server running at port number http://localhost:${PORT}`)
 })
